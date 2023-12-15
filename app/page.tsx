063603406aa5a3040809fade1ad3f9edf531f393
Jose Calderon <josecalderoncelis@gmail.com>
2023-12-15 12:01:35 -0500

    ok, fin cap 7.- Fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import styles from '@/app/ui/home.module.css';
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon, FolderIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
@@ -12,7 +12,6 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        José Calderón Celis
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}></div>
           <p
@@ -32,6 +31,12 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-red-700-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-400 md:text-base"
+          >
+            <span>Dashboard</span> <FolderIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
