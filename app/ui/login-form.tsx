ad888c798805225511390d0415b442ce659909f9
Jose Calderon <josecalderoncelis@gmail.com>
2024-01-02 20:36:25 -0500

    capítulo 15 - Agregó autenticación a su aplicación y protegió las rutas de su panel.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -38,7 +38,6 @@ export default function LoginForm() {
                 required
                 autoComplete="email" // Add this line
               />
-              />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
